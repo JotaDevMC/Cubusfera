09eb071f2ccf8b557b6fb56004c71a35cfcc7d2b
JotaDevMC <jotadevelopermc@gmail.com>
2018-10-20 12:49:41 +0200

    Added custom join message that displays after user login, to make the login message look better

--- src/main/java/com/cubusfera/core/auth/AuthListener.java
+++ src/main/java/com/cubusfera/core/auth/AuthListener.java
@@ -3,6 +3,7 @@ package com.cubusfera.core.auth;
 import com.cubusfera.core.Core;
 import com.cubusfera.core.util.centering.CentralizeHelper;
 import fr.xephi.authme.events.LoginEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -21,15 +22,16 @@ public class AuthListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event){
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(null);
         notLoggedIn.add(event.getPlayer());
         event.getPlayer().sendMessage("");
         CentralizeHelper.sendCenteredMessage(event.getPlayer(), ChatColor.BLUE + "¡Bienvenido a " + ChatColor.BOLD + "Cubusfera" + ChatColor.RESET + ChatColor.BLUE + "!");
         event.getPlayer().sendMessage("");
-        if(core.getAuthmeApi().isRegistered(event.getPlayer().getName())){
+        if (core.getAuthmeApi().isRegistered(event.getPlayer().getName())) {
             CentralizeHelper.sendCenteredMessage(event.getPlayer(), ChatColor.YELLOW + "Inicia sesión usando:");
             CentralizeHelper.sendCenteredMessage(event.getPlayer(), ChatColor.YELLOW + "/login <contraseña>");
-        }else{
+        } else {
             CentralizeHelper.sendCenteredMessage(event.getPlayer(), ChatColor.YELLOW + "Regístrate usando:");
             CentralizeHelper.sendCenteredMessage(event.getPlayer(), ChatColor.YELLOW + "/register <contraseña> <contraseña>");
         }
@@ -39,22 +41,23 @@ public class AuthListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerLogin(LoginEvent event){
-        if(notLoggedIn.contains(event.getPlayer())) notLoggedIn.remove(event.getPlayer());
-        if(core.getPermsApi().getUser(event.getPlayer().getUniqueId()).getPrimaryGroup() == "default"){
+    public void onPlayerLogin(LoginEvent event) {
+        if (notLoggedIn.contains(event.getPlayer())) notLoggedIn.remove(event.getPlayer());
+        if (core.getPermsApi().getUser(event.getPlayer().getUniqueId()).getPrimaryGroup() == "default") {
             event.getPlayer().sendMessage("");
             CentralizeHelper.sendCenteredMessage(event.getPlayer(), ChatColor.BLUE + "¡Bienvenido a " + ChatColor.BOLD + "Cubusfera" + ChatColor.RESET + ChatColor.BLUE + "!");
             event.getPlayer().sendMessage("");
             CentralizeHelper.sendCenteredMessage(event.getPlayer(), ChatColor.YELLOW + "Aún no has sido verificado por un administrador. Para ello,");
             CentralizeHelper.sendCenteredMessage(event.getPlayer(), ChatColor.YELLOW + "asegúrate de haber hablado con un administrador por Discord.");
             event.getPlayer().sendMessage("");
-            CentralizeHelper.sendCenteredMessage(event.getPlayer(), ChatColor.BLUE + "" + ChatColor.BOLD + "discord.cubusfera.com");
+            CentralizeHelper.sendCenteredMessage(event.getPlayer(), ChatColor.BLUE + "" + ChatColor.BOLD + " discord.cubusfera.com");
             event.getPlayer().sendMessage("");
         }
+        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "+" + ChatColor.GRAY + "] " + event.getPlayer().getName());
     }
 
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event){
-        if(notLoggedIn.contains(event.getPlayer())) notLoggedIn.remove(event.getPlayer());
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        if (notLoggedIn.contains(event.getPlayer())) notLoggedIn.remove(event.getPlayer());
     }
 }
